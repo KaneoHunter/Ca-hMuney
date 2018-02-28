14993acf5abceab850a1f767574e8e6b15e099f5
Kaneo <dingdongkane@gmail.com>
2018-02-28 20:42:11 +0000

    am i doing this correctly?

---
+++ Block.h
@@ -0,0 +1,28 @@
+#ifndef CashMuney_BLOCK_H
+#define CashMuney_BLOCK_H
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Block {
+public:
+	string sPrevHash;
+
+	Block(uint32_t nIndexIn, const string &sDataIn);
+
+	string GetHash();
+
+	void MineBlock(uint32_t nDifficulty);
+
+private:
+	uint32_t _nIndex;
+	int64_t _nNonce;
+	string _sData;
+	string _sHash;
+	time_t _tTime;
+
+	string _CalculateHash() const;
+};
+#endif //CashMuney_BLOCK_H
---
+++ README.txt
@@ -0,0 +1,3 @@
+My 1st shitcoin please dont hate lmao
+any actual advice/guidance would be genuinely appreciated
+literally learning cpp as I go so yeah, please send help and cookies
